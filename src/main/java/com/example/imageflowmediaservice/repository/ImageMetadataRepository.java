3f45cfe1179c415ceb5a5aa525ccd760ea09eed0
Giancotsu <gianfrancolarocca2797@gmail.com>
2023-03-01 23:24:21 +0100

    Adding image repository

---
+++ src/main/java/com/example/imageflowmediaservice/repository/ImageMetadataRepository.java
@@ -0,0 +1,7 @@
+package com.example.imageflowmediaservice.repository;
+
+import com.example.imageflowmediaservice.model.ImageMetadata;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ImageMetadataRepository extends MongoRepository<ImageMetadata, String> {
+}
