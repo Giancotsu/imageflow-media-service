32dbdb074ddc7d647bd9914f1b1498f0f60aebf8
Giancotsu <gianfrancolarocca2797@gmail.com>
2023-03-01 23:11:08 +0100

    Init project

---
+++ src/test/java/com/example/imageflowmediaservice/ImageflowMediaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imageflowmediaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageflowMediaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
